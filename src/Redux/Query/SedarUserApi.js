4e69cde1ecbc4d0c464ed9b03417a14abdb9b469
Humer-CA <humeraltares@gmail.com>
2024-04-11 09:43:31 +0800

    sedar active users

--- src/Redux/Query/SedarUserApi.js
+++ src/Redux/Query/SedarUserApi.js
@@ -1,29 +1,28 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const sedarUsersApi = createApi({
-    reducerPath: 'sedarUsersApi',
-    tagTypes: ["User"],
+  reducerPath: "sedarUsersApi",
+  tagTypes: ["User"],
 
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://rdfsedar.com/api",
-        prepareHeaders: (headers) => {
-            const token = process.env.SEDAR_KEY
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://rdfsedar.com/api",
+    prepareHeaders: (headers) => {
+      const token = process.env.SEDAR_KEY;
 
-            headers.set('Authorization', `Bearer ${token}`)
-            headers.set('Accept', `application/json`)
+      headers.set("Authorization", `Bearer ${token}`);
+      headers.set("Accept", `application/json`);
 
-            return headers
-        }
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({
+    getSedarUsersApi: builder.query({
+      query: () => `/data/employee/filter/active`,
+      transformResponse: (response) => response.data,
+      providesTags: ["User"],
     }),
+  }),
+});
 
-    endpoints: (builder) => ({
-        getSedarUsersApi: builder.query({
-            query: () => `/data/employees`,
-            transformResponse: (response) => response.data,
-            providesTags: ["User"]
-        }),
-    }),
-})
-
-
-export const { useGetSedarUsersApiQuery } = sedarUsersApi
+export const { useGetSedarUsersApiQuery, useLazyGetSedarUsersApiQuery } = sedarUsersApi;
