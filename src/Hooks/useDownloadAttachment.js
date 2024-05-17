54f6d2ee0dc2a67e8343c33072c51024c8a419c9
Humer-CA <humeraltares@gmail.com>
2024-05-17 22:21:11 +0800

    added transfer download attachment

--- src/Hooks/useDownloadAttachment.js
+++ src/Hooks/useDownloadAttachment.js
@@ -10,11 +10,33 @@ export const useDownloadAttachment = async ({ attachment, id }) => {
 
     if (!response.ok) {
       // Handle the error if the API response is not successful.
-      throw new Error("Failed to download attachement file.");
+      throw new Error("Failed to download attachment file.");
     }
 
     const blob = await response.blob();
     saveAs(blob, `${attachment}`); // Specify the filename for the downloaded file.
+  } catch (error) {
+    console.error("Error while downloading attachment file:", error);
+  }
+};
+
+export const useDownloadTransferAttachment = async ({ attachments, transfer_number }) => {
+  try {
+    const response = await fetch(`${process.env.VLADIMIR_BASE_URL}/transfer-attachment/${transfer_number}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: " application/json",
+      },
+    });
+
+    if (!response.ok) {
+      // Handle the error if the API response is not successful.
+      throw new Error("Failed to download attachment file.");
+    }
+
+    const blob = await response.blob();
+    saveAs(blob, `${attachments}`); // Specify the filename for the downloaded file.
   } catch (error) {
     console.error("Error while downloading attachment file:", error);
   }
