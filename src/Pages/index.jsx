8b537b18edbd5ed46619b2a1856beeab0b6137e9
Humer-CA <humeraltares@gmail.com>
2024-05-22 17:02:31 +0800

    modified dashboard and added mobileScreen validation

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -42,138 +42,150 @@ const dashboardItems = [
   },
 ];
 
-const CardDashboard = ({ title, icon, data, color }) => (
-  <Stack
-    sx={{
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "center",
-      bgcolor: "white",
-      borderRadius: "10px",
-      width: "fitContent",
-      boxShadow: "10px 10px 20px #dfdfdf,-10px -15px 40px #ffffff",
-      p: "20px",
-      gap: 2,
-      overflow: "auto",
-      maxWidth: "300px",
-    }}
-  >
+const Dashboard = () => {
+  const isSmallScreen = useMediaQuery("(max-width: 1400px)");
+  const isMobileScreen = useMediaQuery("(max-width: 800px)");
+  const userDetails = useSelector((state) => state.userLogin);
+
+  const CardDashboard = ({ title, icon, data, color }) => (
     <Stack
       sx={{
+        flexDirection: "row",
         alignItems: "center",
         justifyContent: "center",
-        bgcolor: `${color}`,
-        p: "10px",
-        borderRadius: "100%",
-      }}
-    >
-      {icon}
-    </Stack>
-
-    <Box width="250px" flexWrap="wrap">
-      <Typography fontWeight={600}>{title}</Typography>
-      <Typography fontSize=".7rem" color="gray">
-        {data}
-      </Typography>
-    </Box>
-  </Stack>
-);
-
-const GraphDashboard = ({ title, icon, data, color }) => (
-  <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center", gap: 4, px: "20px" }}>
-    <Stack
-      gap={1}
-      sx={{
-        p: "20px",
-        height: "200px",
-        width: "60%",
-        maxWidth: "900px",
+        bgcolor: "white",
         borderRadius: "10px",
+        width: "fitContent",
         boxShadow: "10px 10px 20px #dfdfdf,-10px -15px 40px #ffffff",
+        p: "20px",
+        gap: 2,
+        overflow: "auto",
+        maxWidth: isMobileScreen ? "90%" : "300px",
       }}
     >
-      <Stack flexDirection="row" alignItems="center" gap={1}>
-        <BarChartOutlined />
-        <Typography fontWeight={600}>Added Asset</Typography>
+      <Stack
+        sx={{
+          alignItems: "center",
+          justifyContent: "center",
+          bgcolor: `${color}`,
+          p: "10px",
+          borderRadius: "100%",
+        }}
+      >
+        {icon}
       </Stack>
 
-      <BarChart
-        // width={600}
-        // height={140}
-        series={[
-          { data: [35, 44, 24, 34], color: "#ffaf38" },
-          { data: [51, 6, 49, 30], color: "#566d7a" },
-          { data: [15, 25, 30, 50], color: "#ff9900" },
-          { data: [60, 50, 15, 25], color: "#9c958b" },
-        ]}
-        xAxis={[{ data: ["Q1", "Q2", "Q3", "Q4"], scaleType: "band" }]}
-        margin={{ top: 30, bottom: 30, left: 40, right: 10 }}
-      />
+      <Box width="250px" flexWrap="wrap">
+        <Typography fontWeight={600}>{title}</Typography>
+        <Typography fontSize=".7rem" color="gray">
+          {data}
+        </Typography>
+      </Box>
     </Stack>
+  );
 
-    <Stack
-      alignItems="center"
+  const GraphDashboard = ({ title, icon, data, color }) => (
+    <Box
       sx={{
-        height: "200px",
-        width: "20%",
-        p: "20px",
-        boxShadow: "10px 10px 20px #dfdfdf,-10px -15px 40px #ffffff",
-        borderRadius: "10px",
+        display: "flex",
+        flexWrap: "wrap",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 4,
+        px: "20px",
+        flexDirection: "row",
       }}
     >
-      <Stack flexDirection="row" gap={1} alignSelf="flex-start">
-        <AttachMoney />
-        <Typography fontWeight={600} sx={{ textOverflow: "ellipsis" }}>
-          Depreciation
+      <Stack
+        gap={1}
+        sx={{
+          p: "20px",
+          height: "200px",
+          width: isMobileScreen ? "90%" : "60%",
+          maxWidth: "900px",
+          borderRadius: "10px",
+          boxShadow: "10px 10px 20px #dfdfdf,-10px -15px 40px #ffffff",
+        }}
+      >
+        <Stack flexDirection="row" alignItems="center" gap={1}>
+          <BarChartOutlined />
+          <Typography fontWeight={600}>Added Asset</Typography>
+        </Stack>
+
+        <BarChart
+          // width={600}
+          // height={140}
+          series={[
+            { data: [35, 44, 24, 34], color: "#ffaf38" },
+            { data: [51, 6, 49, 30], color: "#566d7a" },
+            { data: [15, 25, 30, 50], color: "#ff9900" },
+            { data: [60, 50, 15, 25], color: "#9c958b" },
+          ]}
+          xAxis={[{ data: ["Q1", "Q2", "Q3", "Q4"], scaleType: "band" }]}
+          margin={{ top: 30, bottom: 30, left: 40, right: 10 }}
+        />
+      </Stack>
+
+      {/* PieChart */}
+      <Stack
+        alignItems="center"
+        sx={{
+          height: "200px",
+          width: isMobileScreen ? "90%" : "30%",
+          maxWidth: isMobileScreen ? "90%" : "350px",
+          p: "20px",
+          boxShadow: "10px 10px 20px #dfdfdf,-10px -15px 40px #ffffff",
+          borderRadius: "10px",
+        }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "row", gap: 1, alignSelf: "flex-start" }}>
+          <AttachMoney />
+          <Typography fontWeight={600} overflow="hidden">
+            Depreciation
+          </Typography>
+        </Box>
+
+        <PieChart
+          series={[
+            {
+              data: [
+                { id: 0, value: 10, label: "series A", color: "#ffaf38" },
+                { id: 1, value: 15, label: "series B", color: "#566d7a" },
+                { id: 2, value: 20, label: "series C", color: "#ff9900" },
+              ],
+              highlightScope: { faded: "global", highlighted: "item" },
+              faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+            },
+          ]}
+          slotProps={{
+            legend: isSmallScreen
+              ? { hidden: true }
+              : {
+                  direction: "column",
+                },
+          }}
+          margin={isSmallScreen ? { top: 1, bottom: 1, left: 1, right: 1 } : null}
+        />
+      </Stack>
+
+      {/* Reports */}
+      <Stack
+        backgroundColor="#ff9900"
+        sx={{
+          height: "200px",
+          width: isMobileScreen ? "90%" : "30%",
+          maxWidth: isMobileScreen ? "90%" : "350px",
+          p: "20px",
+          borderRadius: "10px",
+          boxShadow: "10px 10px 20px #dfdfdf,-10px -15px 40px #ffffff",
+        }}
+      >
+        <Typography fontWeight={600} color="white">
+          Reports
         </Typography>
       </Stack>
-
-      <PieChart
-        series={[
-          {
-            data: [
-              { id: 0, value: 10, label: "series A", color: "#ffaf38" },
-              { id: 1, value: 15, label: "series B", color: "#566d7a" },
-              { id: 2, value: 20, label: "series C", color: "#ff9900" },
-            ],
-            highlightScope: { faded: "global", highlighted: "item" },
-            faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
-          },
-        ]}
-        // width={400}
-        // height={190}
-        slotProps={{
-          legend: {
-            direction: "row",
-            position: { vertical: "bottom", horizontal: "middle" },
-            padding: 0,
-          },
-        }}
-        margin={{ top: 1, bottom: 1, left: 1, right: 1 }}
-        sx={{ pt: "10px" }}
-      />
-    </Stack>
-
-    <Stack
-      backgroundColor="#ff9900"
-      sx={{
-        height: "200px",
-        width: "20%",
-        p: "20px",
-        borderRadius: "10px",
-        boxShadow: "10px 10px 20px #dfdfdf,-10px -15px 40px #ffffff",
-      }}
-    >
-      <Typography fontWeight={600} color="white">
-        Reports
-      </Typography>
-    </Stack>
-  </Box>
-);
-
-const Dashboard = () => {
-  const isSmallScreen = useMediaQuery("max-width: 1400px");
-  const userDetails = useSelector((state) => state.userLogin);
+    </Box>
+  );
 
   return (
     <Box className="mcontainer">
@@ -188,7 +200,7 @@ const Dashboard = () => {
       </Typography>
 
       <Box className="dashboard__main-content">
-        <Box className="dashboard__header" sx={{ mt: "20px" }}>
+        <Box className="dashboard__header" sx={{ mt: "20px", gap: "30px" }}>
           <Box>
             <Typography
               sx={{
@@ -218,9 +230,10 @@ const Dashboard = () => {
             sx={{
               bgcolor: "#f5f5f5cb",
               borderRadius: "100%",
-              width: "180px",
-              height: "180px",
+              width: isMobileScreen ? "120px" : "180px",
+              height: isMobileScreen ? "120px" : "180px",
               position: "relative",
+              display: isMobileScreen > "400px" ? "none" : "flex",
             }}
           >
             <Box
@@ -229,8 +242,8 @@ const Dashboard = () => {
                 left: "-8%",
                 bgcolor: "#f7c708ea",
                 borderRadius: "100%",
-                width: "100px",
-                height: "100px",
+                width: isMobileScreen ? "70px" : "100px",
+                height: isMobileScreen ? "70px" : "100px",
                 position: "absolute",
               }}
             />
@@ -246,7 +259,7 @@ const Dashboard = () => {
               flexWrap: "wrap",
               alignItems: "center",
               justifyContent: "center",
-              flexGrow: 1,
+              // flexGrow: 1,
               flexDirection: "row",
               gap: isSmallScreen ? 2 : 4,
               alignItems: "center",
