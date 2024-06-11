8b4143342a74bf4d47eaa5ab4d36afcb83c076b7
Humer-CA <humeraltares@gmail.com>
2024-06-11 11:25:26 +0800

    added warehouse masterlist

---
+++ src/Pages/Masterlist/Warehouse.jsx
@@ -0,0 +1,366 @@
+import React, { useState } from "react";
+import Moment from "moment";
+import MasterlistToolbar from "../../Components/Reusable/MasterlistToolbar";
+import ActionMenu from "../../Components/Reusable/ActionMenu";
+
+// RTK
+import { useDispatch } from "react-redux";
+import { openToast } from "../../Redux/StateManagement/toastSlice";
+import { openConfirm, closeConfirm, onLoading } from "../../Redux/StateManagement/confirmSlice";
+
+import { usePostWarehouseStatusApiMutation, useGetWarehouseApiQuery } from "../../Redux/Query/Masterlist/Warehouse";
+
+import { useSelector } from "react-redux";
+
+// MUI
+import {
+  Box,
+  Button,
+  Chip,
+  Dialog,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableSortLabel,
+  Typography,
+} from "@mui/material";
+import { Help, ReportProblem } from "@mui/icons-material";
+import MasterlistSkeleton from "../Skeleton/MasterlistSkeleton";
+import ErrorFetching from "../ErrorFetching";
+import AddWarehouse from "./AddEdit/AddWarehouse";
+import NoRecordsFound from "../../Layout/NoRecordsFound";
+import { openDrawer } from "../../Redux/StateManagement/booleanStateSlice";
+
+const Warehouse = () => {
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("active");
+  const [perPage, setPerPage] = useState(5);
+  const [page, setPage] = useState(1);
+  const [updateWarehouse, setUpdateWarehouse] = useState({
+    status: false,
+    id: null,
+    sync_id: null,
+    warehouse_name: "",
+  });
+
+  const drawer = useSelector((state) => state.booleanState.drawer);
+  const dispatch = useDispatch();
+
+  // Table Sorting --------------------------------
+
+  const [order, setOrder] = useState("desc");
+  const [orderBy, setOrderBy] = useState("id");
+
+  const descendingComparator = (a, b, orderBy) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  };
+
+  const comparator = (order, orderBy) => {
+    return order === "desc"
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  };
+
+  const onSort = (property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  // Table Properties --------------------------------
+
+  const perPageHandler = (e) => {
+    setPage(1);
+    setPerPage(parseInt(e.target.value));
+  };
+
+  const pageHandler = (_, page) => {
+    // console.log(page + 1);
+    setPage(page + 1);
+  };
+
+  const {
+    data: warehouseData,
+    isLoading: warehouseLoading,
+    isSuccess: warehouseSuccess,
+    isError: warehouseError,
+    error: errorData,
+    refetch,
+  } = useGetWarehouseApiQuery(
+    {
+      page: page,
+      per_page: perPage,
+      status: status,
+      search: search,
+    },
+    { refetchOnMountOrArgChange: true }
+  );
+
+  const [postWarehouseStatusApi, { isLoading }] = usePostWarehouseStatusApiMutation();
+
+  // console.log(warehouseData);
+
+  const onArchiveRestoreHandler = async (id) => {
+    dispatch(
+      openConfirm({
+        icon: status === "active" ? ReportProblem : Help,
+        iconColor: status === "active" ? "alert" : "info",
+        message: (
+          <Box>
+            <Typography> Are you sure you want to</Typography>
+            <Typography
+              sx={{
+                display: "inline-block",
+                color: "secondary.main",
+                fontWeight: "bold",
+                fontFamily: "Raleway",
+              }}
+            >
+              {status === "active" ? "ARCHIVE" : "ACTIVATE"}
+            </Typography>{" "}
+            this data?
+          </Box>
+        ),
+
+        onConfirm: async () => {
+          try {
+            dispatch(onLoading());
+            const result = await postWarehouseStatusApi({
+              id: id,
+              status: status === "active" ? false : true,
+            }).unwrap();
+
+            dispatch(
+              openToast({
+                message: result.message,
+                duration: 5000,
+              })
+            );
+            dispatch(closeConfirm());
+          } catch (err) {
+            if (err?.status === 422) {
+              dispatch(
+                openToast({
+                  message: err.data.errors?.detail,
+                  duration: 5000,
+                  variant: "error",
+                })
+              );
+            } else if (err?.status !== 422) {
+              dispatch(
+                openToast({
+                  message: "Something went wrong. Please try again.",
+                  duration: 5000,
+                  variant: "error",
+                })
+              );
+            }
+          }
+        },
+      })
+    );
+  };
+
+  const onUpdateHandler = (props) => {
+    const { id, warehouse_name, sync_id } = props;
+    setUpdateWarehouse({
+      status: true,
+      id: id,
+      sync_id: sync_id,
+      warehouse_name: warehouse_name,
+    });
+  };
+
+  const onUpdateResetHandler = () => {
+    setUpdateWarehouse({
+      status: false,
+      id: null,
+      sync_id: [],
+      warehouse_name: "",
+    });
+  };
+
+  const onSetPage = () => {
+    setPage(1);
+  };
+
+  const onViewDepartmentHandler = (props) => {
+    const { id, warehouse_name, sync_id } = props;
+    setUpdateWarehouse({
+      status: true,
+      action: "view",
+      id: id,
+      sync_id: sync_id,
+      warehouse_name: warehouse_name,
+    });
+  };
+
+  // console.log(warehouseData);
+
+  return (
+    <Box className="mcontainer">
+      <Typography className="mcontainer__title" sx={{ fontFamily: "Anton", fontSize: "2rem" }}>
+        Warehouse
+      </Typography>
+
+      {warehouseLoading && <MasterlistSkeleton onAdd={true} />}
+      {warehouseError && <ErrorFetching refetch={refetch} error={errorData} />}
+      {warehouseData && !warehouseError && (
+        <Box className="mcontainer__wrapper">
+          <MasterlistToolbar
+            path="#"
+            onStatusChange={setStatus}
+            onSearchChange={setSearch}
+            onSetPage={setPage}
+            onAdd={() => {}}
+          />
+
+          <Box>
+            <TableContainer className="mcontainer__th-body">
+              <Table className="mcontainer__table" stickyHeader>
+                <TableHead>
+                  <TableRow
+                    sx={{
+                      "& > *": {
+                        fontWeight: "bold!important",
+                        whiteSpace: "nowrap",
+                      },
+                    }}
+                  >
+                    <TableCell className="tbl-cell text-center">
+                      <TableSortLabel
+                        active={orderBy === `id`}
+                        direction={orderBy === `id` ? order : `asc`}
+                        onClick={() => onSort(`id`)}
+                      >
+                        ID No.
+                      </TableSortLabel>
+                    </TableCell>
+
+                    <TableCell className="tbl-cell">
+                      <TableSortLabel
+                        active={orderBy === `warehouse_name`}
+                        direction={orderBy === `warehouse_name` ? order : `asc`}
+                        onClick={() => onSort(`warehouse_name`)}
+                      >
+                        Warehouse
+                      </TableSortLabel>
+                    </TableCell>
+
+                    <TableCell className="tbl-cell text-center">Status</TableCell>
+
+                    <TableCell className="tbl-cell text-center">
+                      <TableSortLabel
+                        active={orderBy === `created_at`}
+                        direction={orderBy === `created_at` ? order : `asc`}
+                        onClick={() => onSort(`created_at`)}
+                      >
+                        Date Created
+                      </TableSortLabel>
+                    </TableCell>
+
+                    <TableCell className="tbl-cell">Action</TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {warehouseData.data.length === 0 ? (
+                    <NoRecordsFound />
+                  ) : (
+                    <>
+                      {warehouseSuccess &&
+                        [...warehouseData.data].sort(comparator(order, orderBy)).map((data) => (
+                          <TableRow
+                            key={data.id}
+                            hover={true}
+                            sx={{
+                              "&:last-child td, &:last-child th": {
+                                borderBottom: 0,
+                              },
+                            }}
+                          >
+                            <TableCell className="tbl-cell tr-cen-pad45">{data.id}</TableCell>
+
+                            <TableCell className="tbl-cell text-weight">{data.warehouse_name}</TableCell>
+
+                            <TableCell className="tbl-cell text-center">
+                              {data.is_active ? (
+                                <Chip
+                                  size="small"
+                                  variant="contained"
+                                  sx={{
+                                    background: "#27ff811f",
+                                    color: "active.dark",
+                                    fontSize: "0.7rem",
+                                    px: 1,
+                                  }}
+                                  label="ACTIVE"
+                                />
+                              ) : (
+                                <Chip
+                                  size="small"
+                                  variant="contained"
+                                  sx={{
+                                    background: "#fc3e3e34",
+                                    color: "error.light",
+                                    fontSize: "0.7rem",
+                                    px: 1,
+                                  }}
+                                  label="INACTIVE"
+                                />
+                              )}
+                            </TableCell>
+
+                            <TableCell className="tbl-cell tr-cen-pad45">
+                              {Moment(data.created_at).format("MMM DD, YYYY")}
+                            </TableCell>
+
+                            <TableCell className="tbl-cell ">
+                              <ActionMenu
+                                status={status}
+                                data={data}
+                                onUpdateHandler={onUpdateHandler}
+                                onArchiveRestoreHandler={onArchiveRestoreHandler}
+                              />
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                    </>
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
+
+          <Box className="mcontainer__pagination">
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 15, { label: "All", value: parseInt(warehouseData?.total) }]}
+              component="div"
+              count={warehouseSuccess ? warehouseData.total : 0}
+              page={warehouseSuccess ? warehouseData.current_page - 1 : 0}
+              rowsPerPage={warehouseSuccess ? parseInt(warehouseData?.per_page) : 5}
+              onPageChange={pageHandler}
+              onRowsPerPageChange={perPageHandler}
+            />
+          </Box>
+        </Box>
+      )}
+
+      <Dialog open={drawer} PaperProps={{ sx: { borderRadius: "10px" } }}>
+        <AddWarehouse data={updateWarehouse} refetch={refetch} onUpdateResetHandler={onUpdateResetHandler} />
+      </Dialog>
+    </Box>
+  );
+};
+
+export default Warehouse;
