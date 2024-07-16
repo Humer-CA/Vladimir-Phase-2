431aac8213f4f50fd5daf738c42abe07c8b17f3e
Humer-CA <humeraltares@gmail.com>
2024-07-16 08:32:08 +0800

    change the ip for printing offline

--- src/Redux/Query/FixedAsset/OfflinePrintingFA.js
+++ src/Redux/Query/FixedAsset/OfflinePrintingFA.js
@@ -1,29 +1,29 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const printOfflineFaApi = createApi({
-    reducerPath: 'printFixedAssetApi',
+  reducerPath: "printFixedAssetApi",
 
-    baseQuery: fetchBaseQuery({
-        baseUrl: `http://10.10.10.11:8000/api`,
+  baseQuery: fetchBaseQuery({
+    baseUrl: `http://10.10.10.6:9000/api`,
 
-        prepareHeaders: (headers) => {
-            const token = localStorage.getItem('token')
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("token");
 
-            headers.set('Authorization', `Bearer ${token}`)
-            headers.set('Accept', `application/json`)
+      headers.set("Authorization", `Bearer ${token}`);
+      headers.set("Accept", `application/json`);
 
-            return headers
-        }
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({
+    postPrintOfflineApi: builder.mutation({
+      query: (params) => ({
+        url: `/fixed-asset/barcode?search=${params.search}&startDate=${params.startDate}&endDate=${params.endDate}`,
+        method: "POST",
+      }),
     }),
+  }),
+});
 
-    endpoints: (builder) => ({
-        postPrintOfflineApi: builder.mutation({
-            query: (params) => ({
-                url: `/fixed-asset/barcode?search=${params.search}&startDate=${params.startDate}&endDate=${params.endDate}`,
-                method: "POST",
-            })
-        }),
-    }),
-})
-
-export const { usePostPrintOfflineApiMutation } = printOfflineFaApi
+export const { usePostPrintOfflineApiMutation } = printOfflineFaApi;
