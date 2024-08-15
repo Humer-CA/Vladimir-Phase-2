58423192cb10cf94185a03f83b2a69a2f2fb1ab3
Humer-CA <humeraltares@gmail.com>
2024-08-15 09:17:52 +0800

    done cards transition

--- src/Components/Reusable/Cards.jsx
+++ src/Components/Reusable/Cards.jsx
@@ -1,18 +1,5 @@
 import React from "react";
-import { Box } from "@mui/system";
-
-import {
-  Badge,
-  Button,
-  Card,
-  CardActionArea,
-  CardContent,
-  IconButton,
-  Stack,
-  Typography,
-  useMediaQuery,
-} from "@mui/material";
-
+import { Card, CardActionArea, CardContent, Typography, IconButton, Badge, Stack, useMediaQuery } from "@mui/material";
 import { ArrowForward } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 
@@ -22,38 +9,44 @@ const Cards = (props) => {
   const isSmallScreen = useMediaQuery("(max-width: 590px)");
 
   return (
-    <Card className="parentSidebar__card" onClick={() => navigate(`${data.path}`)}>
-      <CardActionArea component="div" className="parentSidebar__action-area">
+    <Card className="parentSidebar__card slide-in-top" sx={{ transition: ".25s ease-in-out" }}>
+      <CardActionArea
+        onClick={() => navigate(`${data.path}`)}
+        sx={{
+          height: "100%",
+          ".MuiCardActionArea-focusHighlight": {
+            background: "transparent",
+          },
+        }}
+      >
         <CardContent className="parentSidebar__content">
-          {isSmallScreen ? null : (
+          {!isSmallScreen && (
             <Badge color="error" badgeContent={data?.notification}>
               <IconButton className="parentSidebar__icon">{data.icon}</IconButton>
             </Badge>
           )}
           <Stack flexDirection="column" gap={0.2} alignItems="flex-start">
             <Stack flexDirection="row" alignItems="center" gap={2}>
-              {isSmallScreen ? <IconButton className="parentSidebar__icon">{data.icon}</IconButton> : null}
-
+              {isSmallScreen && <IconButton className="parentSidebar__icon">{data.icon}</IconButton>}
               <Stack>
                 <Typography component="div" fontWeight="bold" color="secondary">
                   {data.label}
                 </Typography>
-
-                {isSmallScreen ? (
+                {isSmallScreen && (
                   <Typography variant="span" color="text.secondary" fontSize="12px">
                     {data.description}
                   </Typography>
-                ) : null}
+                )}
               </Stack>
             </Stack>
-            {isSmallScreen ? null : (
+            {!isSmallScreen && (
               <Typography variant="body2" color="text.secondary" fontSize="12px">
                 {data.description}
               </Typography>
             )}
           </Stack>
 
-          {isSmallScreen ? null : (
+          {!isSmallScreen && (
             <IconButton
               size="small"
               sx={{
@@ -62,7 +55,6 @@ const Cards = (props) => {
                   backgroundColor: "secondary.main",
                   color: "white",
                 },
-
                 backgroundColor: "secondary.main",
                 color: "white",
               }}
