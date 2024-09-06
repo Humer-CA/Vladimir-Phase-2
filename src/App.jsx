274c9d93f802aa71b10be1ea1048257a006319ed
Humer-CA <humeraltares@gmail.com>
2024-09-06 11:43:02 +0800

    added user data and permissions

--- src/App.jsx
+++ src/App.jsx
@@ -82,6 +82,9 @@ import ViewPullout from "./Pages/Approving/Pullout/ViewPullout";
 import RrSummary from "./Pages/Asset Requisition/RrSummary";
 import AdditionalCost from "./Pages/FixedAssets/Additional Cost/AdditionalCost";
 
+const userData = JSON.parse(localStorage.getItem("user"));
+const userRole = userData.role.access_permission.split(", ");
+
 const router = createBrowserRouter([
   {
     path: "*",
@@ -100,16 +103,19 @@ const router = createBrowserRouter([
           {
             path: "/",
             element: <Dashboard />,
+            handle: { permission: "dashboard" },
           },
 
           {
             path: "/printer-ip-configuration",
             element: <IpSetup />,
+            handle: { permission: "print-fa" },
           },
 
           {
             path: "masterlist",
             element: <Masterlist />,
+            handle: { permission: "masterlist" },
             children: [
               {
                 index: true,
@@ -119,66 +125,82 @@ const router = createBrowserRouter([
               {
                 path: "company",
                 element: <Company />,
+                handle: { permission: "company" },
               },
               {
                 path: "business-unit",
                 element: <BusinessUnit />,
+                handle: { permission: "business-unit" },
               },
               {
                 path: "department",
                 element: <Department />,
+                handle: { permission: "department" },
               },
               {
                 path: "unit",
                 element: <Unit />,
+                handle: { permission: "unit" },
               },
               {
                 path: "sub-unit",
                 element: <SubUnit />,
+                handle: { permission: "sub-unit" },
               },
               {
                 path: "location",
                 element: <Location />,
+                handle: { permission: "location" },
               },
               {
                 path: "account-title",
                 element: <AccountTitle />,
+                handle: { permission: "account-title" },
               },
               {
                 path: "supplier",
                 element: <Supplier />,
+                handle: { permission: "supplier" },
               },
               {
                 path: "division",
                 element: <Division />,
+                handle: { permission: "division" },
               },
               {
                 path: "type-of-request",
                 element: <TypeOfRequest />,
+                handle: { permission: "type-of-request" },
               },
               {
                 path: "capex",
                 element: <Capex />,
+                handle: { permission: "capex" },
               },
               {
                 path: "warehouse",
                 element: <Warehouse />,
+                handle: { permission: "warehouse" },
               },
               {
                 path: "service-provider",
                 element: <ServiceProvider />,
+                handle: { permission: "service-provider" },
               },
               {
                 path: "category",
                 element: <Category />,
+                handle: { permission: "category" },
               },
               {
                 path: "status-category",
                 element: <FaStatusCategory />,
+                handle: { permission: "status-category" },
               },
               {
                 path: "unit-of-measurement",
                 element: <UnitOfMeasurement />,
+                handle: { permission: "unit-of-measurement" },
               },
 
               // {
@@ -191,6 +213,7 @@ const router = createBrowserRouter([
           {
             path: "user-management",
             element: <UserManagement />,
+            handle: { permission: "user-management" },
             children: [
               {
                 index: true,
@@ -200,10 +223,12 @@ const router = createBrowserRouter([
               {
                 path: "user-accounts",
                 element: <UserAccounts />,
+                handle: { permission: "user-accounts" },
               },
               {
                 path: "role-management",
                 element: <RoleManagement />,
+                handle: { permission: "role-management" },
               },
             ],
           },
@@ -212,22 +237,26 @@ const router = createBrowserRouter([
             path: "fixed-asset",
             index: false,
             element: <FixedAsset />,
+            handle: { permission: "fixed-asset" },
             children: [],
           },
 
           {
             path: "fixed-asset/:tag_number",
             element: <FixedAssetView />,
+            handle: { permission: "fixed-asset" },
           },
 
           {
             path: "fixed-asset/additional-cost",
             element: <AdditionalCost />,
+            handle: { permission: "fixed-asset" },
           },
 
           {
             path: "settings",
             element: <Settings />,
+            handle: { permission: "settings" },
             children: [
               {
                 index: true,
@@ -237,10 +266,12 @@ const router = createBrowserRouter([
               {
                 path: "approver-settings",
                 element: <ApproverSettings />,
+                handle: { permission: "settings" },
               },
               {
                 path: "form-settings",
                 element: <FormSettings />,
+                handle: { permission: "settings" },
               },
             ],
           },
@@ -248,63 +279,77 @@ const router = createBrowserRouter([
           {
             path: "asset-requisition",
             element: <AssetRequisition />,
+            handle: { permission: "asset-requisition" },
             children: [
               {
                 path: "requisition",
                 element: <Requisition />,
+                handle: { permission: "requisition" },
               },
 
               // Request
               {
                 path: "requisition/add-requisition",
                 element: <AddRequisition />,
+                handle: { permission: "requisition" },
               },
               {
                 path: "requisition/view-requisition/:transaction_number",
                 element: <AddRequisition />,
+                handle: { permission: "requisition" },
               },
               {
                 path: "requisition/edit-requisition/:transaction_number",
                 element: <AddRequisition />,
+                handle: { permission: "requisition" },
               },
 
               // Additional Cost
               {
                 path: "requisition/additional-cost",
                 element: <AdditionalCostRequest />,
+                handle: { permission: "requisition" },
               },
               {
                 path: "requisition/additional-cost/:transaction_number",
                 element: <AdditionalCostRequest />,
+                handle: { permission: "requisition" },
               },
 
               {
                 path: "purchase-request",
                 element: <PurchaseRequest />,
+                handle: { permission: "purchase-request" },
               },
               {
                 path: "purchase-request/:transaction_number",
                 element: <ViewRequestPr />,
+                handle: { permission: "purchase-request" },
               },
               {
                 path: "requisition-received-asset",
                 element: <ReceivedAsset />,
+                handle: { permission: "requisition-received-asset" },
               },
               {
                 path: "requisition-received-asset/:transaction_number",
                 element: <ViewRequestReceiving />,
+                handle: { permission: "requisition-received-asset" },
               },
               {
                 path: "requisition-rr-summary",
                 element: <RrSummary />,
+                handle: { permission: "requisition-rr-summary" },
               },
               {
                 path: "requisition-releasing",
                 element: <ReleasingOfAsset />,
+                handle: { permission: "requisition-releasing" },
               },
               {
                 path: "requisition-releasing/:warehouse_number",
                 element: <ViewRequestReleasing />,
+                handle: { permission: "requisition-releasing" },
               },
             ],
           },
@@ -322,26 +367,32 @@ const router = createBrowserRouter([
           {
             path: "asset-movement",
             element: <AssetMovement />,
+            handle: { permission: "asset-movement" },
             children: [
               {
                 path: "transfer",
                 element: <Transfer />,
+                handle: { permission: "transfer" },
               },
               {
                 path: "transfer/add-transfer",
                 element: <AddTransfer />,
+                handle: { permission: "transfer" },
               },
               {
                 path: "transfer/add-transfer/:transfer_number",
                 element: <AddTransfer />,
+                handle: { permission: "transfer" },
               },
               {
                 path: "pull-out",
                 // element: <ServiceProvider />,
+                handle: { permission: "pull-out" },
               },
               {
                 path: "disposal",
                 // element: <Category />,
+                handle: { permission: "disposal" },
               },
             ],
           },
@@ -349,38 +400,47 @@ const router = createBrowserRouter([
           {
             path: "approving",
             element: <Approving />,
+            handle: { permission: "approving" },
             children: [
               {
                 path: "request",
                 element: <RequestApproving />,
+                handle: { permission: "approving" },
               },
               {
                 path: "request/:transaction_number",
                 element: <ViewApproveRequest />,
+                handle: { permission: "approving" },
               },
               {
                 path: "transfer",
                 element: <TransferApproving />,
+                handle: { permission: "approving" },
               },
               {
                 path: "transfer/:transfer_number",
                 element: <ViewTransfer />,
+                handle: { permission: "approving" },
               },
               {
                 path: "pull-out",
                 element: <PulloutApproval />,
+                handle: { permission: "approving" },
               },
               {
                 path: "pullout/:transaction_number",
                 element: <ViewPullout />,
+                handle: { permission: "approving" },
               },
               {
                 path: "disposal",
                 element: <DisposalApproving />,
+                handle: { permission: "approving" },
               },
               {
                 path: "disposal/:transaction_number",
                 element: <ViewApproveRequest />,
+                handle: { permission: "approving" },
               },
             ],
           },
@@ -388,43 +448,52 @@ const router = createBrowserRouter([
           {
             path: "request-monitoring",
             element: <RequestMonitoring />,
+            handle: { permission: "request-monitoring" },
           },
 
           {
             path: "request-monitoring/:transaction_number",
             element: <ViewRequest />,
+            handle: { permission: "request-monitoring" },
           },
 
           {
             path: "asset-for-tagging",
             // element: <AssetForTagging />,
+            handle: { permission: "asset-for-tagging" },
           },
 
           {
             path: "asset-list",
             // element: <AssetList />,
+            handle: { permission: "asset-list" },
           },
 
           {
             path: "on-hand-in-process",
             // element: <OnHandInProcess />,
+            handle: { permission: "on-hand-in-process" },
           },
 
           {
             path: "reports",
             // element: <Reports />,
+            handle: { permission: "reports" },
             children: [
               {
                 path: "report1",
                 // element: <UserAccounts />,
+                handle: { permission: "report1" },
               },
               {
                 path: "report2",
                 // element: <ServiceProvider />,
+                handle: { permission: "report2" },
               },
               {
                 path: "report3",
                 // element: <Category />,
+                handle: { permission: "report2" },
               },
             ],
           },
