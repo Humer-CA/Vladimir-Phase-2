b676d53cc23ee9290e99f41f13136a9f17f9e74c
Humer-CA <humeraltares@gmail.com>
2024-10-08 08:22:34 +0800

    New MajorCategory using InjectEndpoints

--- src/Redux/Query/Masterlist/Category/MajorCategory.js
+++ src/Redux/Query/Masterlist/Category/MajorCategory.js
@@ -1,71 +1,133 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const majorCategoryApi = createApi({
-  reducerPath: "majorCategoryApi",
-  tagTypes: ["majorCategory"],
+// export const majorCategoryApi = createApi({
+//   reducerPath: "majorCategoryApi",
+//   tagTypes: ["majorCategory"],
 
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.VLADIMIR_BASE_URL,
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: process.env.VLADIMIR_BASE_URL,
 
-    prepareHeaders: (headers) => {
-      const token = localStorage.getItem("token");
+//     prepareHeaders: (headers) => {
+//       const token = localStorage.getItem("token");
 
-      headers.set("Authorization", `Bearer ${token}`);
-      headers.set("Accept", `application/json`);
+//       headers.set("Authorization", `Bearer ${token}`);
+//       headers.set("Accept", `application/json`);
 
-      return headers;
-    },
-  }),
+//       return headers;
+//     },
+//   }),
 
-  endpoints: (builder) => ({
-    getMajorCategoryApi: builder.query({
-      query: (params) =>
-        `/major-category?search=${params.search}&page=${params.page}&per_page=${params.per_page}&status=${params.status}`,
-      providesTags: ["majorCategory"],
-    }),
+//   endpoints: (builder) => ({
+//     getMajorCategoryApi: builder.query({
+//       query: (params) =>
+//         `/major-category?search=${params.search}&page=${params.page}&per_page=${params.per_page}&status=${params.status}`,
+//       providesTags: ["majorCategory"],
+//     }),
 
-    getMajorCategoryAllApi: builder.query({
-      query: () => `/major-category?pagination=none`,
-      // transformResponse: (response) => response.data,
-      providesTags: ["majorCategory"],
-    }),
+//     getMajorCategoryAllApi: builder.query({
+//       query: () => `/major-category?pagination=none`,
+//       // transformResponse: (response) => response.data,
+//       providesTags: ["majorCategory"],
+//     }),
 
-    getMajorCategoryIdApi: builder.query({
-      query: (id) => `/major-category/${id}`,
-      providesTags: ["majorCategory"],
-    }),
+//     getMajorCategoryIdApi: builder.query({
+//       query: (id) => `/major-category/${id}`,
+//       providesTags: ["majorCategory"],
+//     }),
 
-    putMajorCategoryStatusApi: builder.mutation({
-      query: ({ id, status }) => ({
-        url: `/major-category/archived-major-category/${id}`,
-        method: "PUT",
-        body: {
-          status: status,
-        },
+//     putMajorCategoryStatusApi: builder.mutation({
+//       query: ({ id, status }) => ({
+//         url: `/major-category/archived-major-category/${id}`,
+//         method: "PUT",
+//         body: {
+//           status: status,
+//         },
+//       }),
+//       invalidatesTags: ["majorCategory"],
+//     }),
+
+//     postMajorCategoryApi: builder.mutation({
+//       query: (data) => ({
+//         url: `/major-category`,
+//         method: "POST",
+//         body: data,
+//       }),
+//       invalidatesTags: ["majorCategory"],
+//     }),
+
+//     updateMajorCategoryApi: builder.mutation({
+//       query: ({ id, ...data }) => ({
+//         url: `/major-category/${id}`,
+//         method: "PUT",
+//         body: data,
+//       }),
+//       invalidatesTags: ["majorCategory"],
+//     }),
+//   }),
+// });
+
+// export const {
+//   useGetMajorCategoryApiQuery,
+//   useGetMajorCategoryAllApiQuery,
+//   useLazyGetMajorCategoryAllApiQuery,
+//   useGetMajorCategoryIdApiQuery,
+//   usePutMajorCategoryStatusApiMutation,
+//   usePostMajorCategoryApiMutation,
+//   useUpdateMajorCategoryApiMutation,
+// } = majorCategoryApi;
+
+import { masterlistApi } from "../Masterlist";
+
+const apiTags = (result, error) => (error ? [] : ["masterlistApi"]);
+
+export const majorCategoryApi = masterlistApi
+  .enhanceEndpoints({
+    addTagTypes: ["majorCategory"],
+  })
+  .injectEndpoints({
+    endpoints: (builder) => ({
+      getMajorCategoryApi: builder.query({
+        query: (params) =>
+          `/major-category?search=${params.search}&page=${params.page}&per_page=${params.per_page}&status=${params.status}`,
+        providesTags: apiTags,
       }),
-      invalidatesTags: ["majorCategory"],
-    }),
-
-    postMajorCategoryApi: builder.mutation({
-      query: (data) => ({
-        url: `/major-category`,
-        method: "POST",
-        body: data,
+      getMajorCategoryAllApi: builder.query({
+        query: () => `/major-category?pagination=none`,
+        providesTags: apiTags,
       }),
-      invalidatesTags: ["majorCategory"],
-    }),
-
-    updateMajorCategoryApi: builder.mutation({
-      query: ({ id, ...data }) => ({
-        url: `/major-category/${id}`,
-        method: "PUT",
-        body: data,
+      getMajorCategoryIdApi: builder.query({
+        query: (id) => `/major-category/${id}`,
+        providesTags: apiTags,
       }),
-      invalidatesTags: ["majorCategory"],
+      putMajorCategoryStatusApi: builder.mutation({
+        query: ({ id, status }) => ({
+          url: `/major-category/archived-major-category/${id}`,
+          method: "PUT",
+          body: { status },
+        }),
+        invalidatesTags: ["majorCategory"], // Invalidate on mutation
+      }),
+      postMajorCategoryApi: builder.mutation({
+        query: (data) => ({
+          url: `/major-category`,
+          method: "POST",
+          body: data,
+        }),
+        invalidatesTags: ["majorCategory"],
+      }),
+      updateMajorCategoryApi: builder.mutation({
+        query: ({ id, ...data }) => ({
+          url: `/major-category/${id}`,
+          method: "PUT",
+          body: data,
+        }),
+        invalidatesTags: ["majorCategory"],
+      }),
     }),
-  }),
-});
+    overrideExisting: false,
+  });
 
+// Export hooks for the queries and mutations
 export const {
   useGetMajorCategoryApiQuery,
   useGetMajorCategoryAllApiQuery,
